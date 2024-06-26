d4894b8b05b49700e109be39172093f7660054c8
ahmadmhr <ahmadamjad539@gmail.com>
2024-06-26 15:07:42 +0500

    update

--- SwiftUI-Weather/ContentView.swift
+++ SwiftUI-Weather/ContentView.swift
@@ -28,7 +28,7 @@ struct ContentView: View {
                 Button{
                     isNight.toggle()
                 }label: {
-                    buttonText(title: "Change Day Time",background: .white,foreground: .green)
+                    buttonText(title: "Change Day Time",background: .white,foreground: .blue)
                 }
                 Spacer()
             }
